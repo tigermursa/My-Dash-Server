654e81a99b889b2ddc956676ec30d56945aef4e1
Mursalin <mursalin2311@gmail.com>
2025-01-17 02:22:34 +0600

    crud is ok for the nav items

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
-import morgan from "morgan";
+import morgan from 'morgan';
 import path from 'path';
-import userRoutes from './app/modules/user/user.routes';
+
 import { errorHandler } from './app/middleware/ErrorHangler';
+import navItemRoutes from './app/modules/nav-items/nav-item.route';
 
 const app: Application = express();
 
 // Middleware for logging
-app.use(morgan("dev"));  // 'dev' outputs concise colored logs
+app.use(morgan('dev')); // 'dev' outputs concise colored logs
 
 // Enable CORS for all origins
 app.use(cors());
@@ -17,21 +18,21 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/v1', userRoutes);
+app.use('/api/v1/nav-items', navItemRoutes);
 
 // Root route
 app.get('/', (_req: Request, res: Response) => {
-    const filePath = path.join(process.cwd(), 'views', 'status.html');
-    res.sendFile(filePath);
+  const filePath = path.join(process.cwd(), 'views', 'status.html');
+  res.sendFile(filePath);
 });
 
 // Global error handling middleware
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(err); // Log the error for debugging
-    res.status(err.status || 500).json({
-        success: false,
-        message: err.message || 'Internal Server Error',
-    });
+  console.error(err); // Log the error for debugging
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+  });
 });
 
 // Error Handler (ensure it's last)
--- src/app/modules/nav-items/nav-item.route.ts
+++ src/app/modules/nav-items/nav-item.route.ts
@@ -7,21 +7,21 @@ import {
   updateNavItemByIDController,
 } from './nav-item.controller';
 
-const router = Router();
+const navItemRoutes = Router();
 
 // Create a nav item
-router.post('/create-nav-item', createNavItemController);
+navItemRoutes.post('/create-nav-item', createNavItemController);
 
 // Get all nav items
-router.get('/get-all-nav-items', getAllNavItemsController);
+navItemRoutes.get('/get-all-nav-items', getAllNavItemsController);
 
 // Get a single nav item by ID
-router.get('/get-nav-item/:id', getNavItemByIDController);
+navItemRoutes.get('/get-nav-item/:id', getNavItemByIDController);
 
 // Delete a nav item by ID
-router.delete('/delete-nav-item/:id', deleteNavItemByIDController);
+navItemRoutes.delete('/delete-nav-item/:id', deleteNavItemByIDController);
 
 // Update a nav item by ID
-router.put('/update-nav-item/:id', updateNavItemByIDController);
+navItemRoutes.put('/update-nav-item/:id', updateNavItemByIDController);
 
-export default router;
+export default navItemRoutes;
