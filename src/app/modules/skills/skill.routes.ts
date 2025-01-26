f09018594d547d684beafce4294ed7975dd24151
Mursalin <mursalin2311@gmail.com>
2025-01-26 20:07:35 +0600

    tested api using postman found many issues

--- src/app/modules/skills/skill.routes.ts
+++ src/app/modules/skills/skill.routes.ts
@@ -9,15 +9,15 @@ import {
 const SkillRouter = Router();
 
 // Route to get all skills for a user
-SkillRouter.get('/get-skills', getAllSkills);
+SkillRouter.get('/get-skills', getAllSkills); //! not ok have to improve the response
 
 // Route to create a new skill
-SkillRouter.post('/create', createSkill);
+SkillRouter.post('/create', createSkill); //! not ok have to improve the response
 
 // Route to update a specific skill
-SkillRouter.put('/update/:id', updateSkill);
+SkillRouter.put('/update/:id', updateSkill); //! not ok have to improve the response
 
 // Route to delete a specific skill
-SkillRouter.delete('/delete/:id', deleteSkill);
+SkillRouter.delete('/delete/:id', deleteSkill); //! not ok have to improve the response
 
 export default SkillRouter;
