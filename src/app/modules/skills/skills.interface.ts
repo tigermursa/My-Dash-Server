7fb9f4274f2cd51db9d12c0e5239f74870e3f8d9
Mursalin <mursalin2311@gmail.com>
2025-02-16 02:30:22 +0600

    updated skill interface

--- src/app/modules/skills/skills.interface.ts
+++ src/app/modules/skills/skills.interface.ts
@@ -1,6 +1,12 @@
 export interface ISkill {
   userID: string;
   skillName: string;
-  category: 'frontend' | 'backend' | 'tool' | 'plan-to-learn' | 'extra';
+  category:
+    | 'frontend'
+    | 'backend'
+    | 'tool'
+    | 'plan-to-learn'
+    | 'extra'
+    | 'language';
   level: 'beginner' | 'medium' | 'advanced';
 }
