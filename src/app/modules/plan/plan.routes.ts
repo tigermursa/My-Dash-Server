9f7557a3c3a5ebc569e0162b29639c7e1469424e
Mursalin <mursalin2311@gmail.com>
2025-07-12 01:46:22 +0600

    updated routes

--- src/app/modules/plan/plan.routes.ts
+++ src/app/modules/plan/plan.routes.ts
@@ -7,6 +7,7 @@ import {
   getTasksHandler,
   updateTaskTextHandler,
 } from './plan.controller';
+import { authenticateToken } from '../../middleware/authMiddleware';
 
 const PlanRoutes = express.Router();
 
@@ -15,6 +16,6 @@ PlanRoutes.patch('/tasks/important', toggleTaskFieldHandler('important'));
 PlanRoutes.patch('/tasks/completed', toggleTaskFieldHandler('isCompleted'));
 PlanRoutes.patch('/tasks/text', updateTaskTextHandler);
 PlanRoutes.delete('/tasks/delete', deleteTaskHandler);
-PlanRoutes.get('/tasks/:userID', getTasksHandler);
+PlanRoutes.get('/tasks/:userID', authenticateToken, getTasksHandler);
 
 export default PlanRoutes;
